37d4f9013d1c232615e626450c656998bb5a57b8
ateixivi14 <ateixivi14@gmail.com>
2023-05-25 10:12:58 +0200

    add exception

--- src/main/java/com/example/springsecuritytraining/SpringSecurityTrainingApplication.java
+++ src/main/java/com/example/springsecuritytraining/SpringSecurityTrainingApplication.java
@@ -1,34 +1,13 @@
 package com.example.springsecuritytraining;
 
-import com.example.springsecuritytraining.domain.ApiUser;
-import com.example.springsecuritytraining.domain.Role;
-import com.example.springsecuritytraining.service.UserService;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
 
 @SpringBootApplication
 public class SpringSecurityTrainingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringSecurityTrainingApplication.class, args);
-    }
-    
-    @Bean
-    CommandLineRunner run(UserService userService) {
-        return args -> {
-            userService.saveRole(new Role(1L, "ROLE_ADMIN"));
-            userService.saveRole(new Role(2L, "ROLE_USER"));
-            
-            userService.saveUser(new ApiUser(1L, "ateixido", "alba", "1234", new ArrayList<>()));
-            userService.saveUser(new ApiUser(2L, "lpascual", "lucas", "1234", new ArrayList<>()));
-            
-            userService.addRoleToUser("lpascual", "ROLE_USER");
-            userService.addRoleToUser("ateixido", "ROLE_ADMIN");
-        };
     }
 
 }
--- src/main/java/com/example/springsecuritytraining/jwt/JwtGenerator.java
+++ src/main/java/com/example/springsecuritytraining/jwt/JwtGenerator.java
@@ -3,23 +3,15 @@ package com.example.springsecuritytraining.jwt;
 import com.nimbusds.jose.*;
 import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.DefaultClaims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.jwt.JwtException;
 import org.springframework.stereotype.Component;
 
-import javax.crypto.spec.SecretKeySpec;
-import javax.xml.bind.DatatypeConverter;
-import java.security.Key;
 import java.text.ParseException;
 import java.time.Instant;
 import java.util.Date;
-import java.util.Map;
 import java.util.stream.Collectors;
 
 @Component
